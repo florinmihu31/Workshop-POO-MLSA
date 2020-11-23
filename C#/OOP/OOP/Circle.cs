612ab69e6535269358662229e3ba8912382821a7
Florin <florinmihu31@gmail.com>
2020-11-23 10:47:13 +0200

    Add interface demo

---
+++ C#/OOP/OOP/Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing circle");
+        }
+    }
+}
